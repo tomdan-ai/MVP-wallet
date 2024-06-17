a570cca58a130d314c6e3886c93f48bf09487376
tomdan-ai <tomudoh258@gmail.com>
2024-06-17 14:43:55 +0100

    Write Schema

--- src/migrations/20240617121415_users_table.ts
+++ src/migrations/20240617121415_users_table.ts
@@ -1,10 +1,18 @@
-import type { Knex } from "knex";
+import { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('users', (table) => {
+        table.increments('id').primary();
+        table.string('email').notNullable().unique();
+        table.string('password').notNullable();
+        table.decimal('balance', 14,2).defaultTo(0);
+        table.timestamps(true, true);
+    });
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable('users');
 }
 
--- src/migrations/20240617121620_blacklist_table.ts
+++ src/migrations/20240617121620_blacklist_table.ts
@@ -2,9 +2,15 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+    return knex.schema.createTable('blacklist', (table) => {
+        table.increments('id').primary();
+        table.string('email').notNullable().unique();
+        table.timestamps(true, true);
+    });
 }
 
 
 export async function down(knex: Knex): Promise<void> {
+    return knex.schema.dropTable('blacklist');
 }
 
