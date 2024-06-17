221d2d6ae866be4824409f332e1f1e67fc37f00c
tomdan-ai <tomudoh258@gmail.com>
2024-06-17 13:04:18 +0100

    Write configurations for using knex and mysql

---
+++ src/knexfile.ts
@@ -0,0 +1,24 @@
+import { Knex } from "knex";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: { [key:string]: Knex.Config} = {
+    development: {
+        client: 'mysql2',
+        connection: {
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+        },
+        migrations: {
+            directory: './src/migrations',
+        },
+        seeds: {
+            directory: './src/seeds',
+        },
+    },
+};
+
+module.exports = config;
