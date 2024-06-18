41fea02e7c7e9fdba74b9045b050ec680de36887
tomdan-ai <tomudoh258@gmail.com>
2024-06-18 21:25:51 +0100

    Add UUID to as ID/Primary key in schema

--- src/migrations/20240617121415_users_table.ts
+++ src/migrations/20240617121415_users_table.ts
@@ -1,18 +1,16 @@
-import { Knex } from "knex";
-
+import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-    return knex.schema.createTable('users', (table) => {
-        table.increments('id').primary();
-        table.string('email').notNullable().unique();
-        table.string('password').notNullable();
-        table.decimal('balance', 14,2).defaultTo(0);
-        table.timestamps(true, true);
-    });
+  return knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary().defaultTo(knex.raw('(UUID())'));
+    table.string('email').notNullable().unique();
+    table.string('password').notNullable();
+    table.decimal('balance', 14, 2).defaultTo(0);
+    table.string('token').unique();
+    table.timestamps(true, true);
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
-    return knex.schema.dropTable('users');
+  return knex.schema.dropTable('users');
 }
-
