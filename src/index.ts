95bda42e0ae8f876a1af6388b515aee13b839d26
tomdan-ai <tomudoh258@gmail.com>
2024-06-18 21:35:14 +0100

    Import new changes in express app

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 
+import authRoutes from './routes/authRoutes';
 import userRoutes from './routes/UserRoutes';
 import transactionRoutes from './routes/TransactionRoutes';
 
@@ -12,8 +13,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.use('/api', userRoutes);
-app.use('/api', transactionRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/transactions', transactionRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
