1bef8f84587096da19a012a53f6445dfac5ccd39
tomdan-ai <tomudoh258@gmail.com>
2024-06-18 11:29:46 +0100

    Remove local and placholder Blacklist

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,19 +1,15 @@
 import { UserModel } from '../models/User';
-import { BlacklistModel } from '../models/Blacklist';
 import { User } from '../models/User';
 
 export class UserService {
   private userModel: UserModel;
-  private blacklistModel: BlacklistModel;
 
   constructor() {
     this.userModel = new UserModel();
-    this.blacklistModel = new BlacklistModel();
   }
 
   public async createUser(user: Omit<User, 'id'>): Promise<User | null> {
-    const isBlacklisted = await this.blacklistModel.findByEmail(user.email);
-    if (isBlacklisted) {
+    if (user) {
       throw new Error('User is blacklisted');
     }
     return this.userModel.create(user);
